5aa1ff092c168a2c6dda4ac5d974b36ffdb90a19
Giorgi Solomnishvili <giorgisolomnishvili349@gmail.com>
2022-09-28 15:48:21 +0200

    modify datapath

--- vivado_assignment_5/assignment_5/assignment_5.srcs/sources_1/new/adder_datapath.vhd
+++ vivado_assignment_5/assignment_5/assignment_5.srcs/sources_1/new/adder_datapath.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
 -- Company    :  NTNU
--- Engineer   : Øystein Gjermundnes
+-- Engineer   : ï¿½ystein Gjermundnes
 -- 
 -- Create Date: 09/11/2016 04:21:16 PM
 -- Module Name: mega_adder_datapath
@@ -40,11 +40,14 @@ end adder_datapath;
 architecture rtl of adder_datapath is
 
   -- Signals associated with the input registers
-  signal a_r, a_nxt: std_logic_vector(127 downto 0);
-  signal b_r, b_nxt: std_logic_vector(127 downto 0);
+  signal a_r, a_rb,  a_nxt: std_logic_vector(127 downto 0);
+  signal b_r, b_rb, b_nxt: std_logic_vector(127 downto 0);
   
   -- Signals associated with the output registers
-  signal y_r, y_nxt: std_logic_vector(127 downto 0);
+  signal y_r, tmp : std_logic_vector(32 downto 0);
+  
+  signal cnt : std_logic_vector(1 downto 0);
+  signal en : std_logic;
     
 begin
 
@@ -73,24 +76,46 @@ begin
   -- Add the content of a_r and b_r and store it in y_r.
   -- Logic for shifting out the content of y_r to data_out
   -- ***************************************************************************
+  
   process (clk, reset_n) begin
     if(reset_n = '0') then
-      y_r <= (others => '0');     
+      a_rb <= (others => '0');    
+      b_rb <= (others => '0'); 
+      tmp <= (others => '0');   
     elsif(clk'event and clk='1') then
-      if(output_reg_en ='1') then
-        y_r <= y_nxt;       
+      if((output_reg_en ='1') and (output_reg_load = '1')) then
+        a_rb <= a_r;
+        b_rb <= b_r;     
+        tmp <= std_logic_vector(unsigned('0'&a_r(31 downto 0)) + unsigned('0'&b_r(31 downto 0)));   
       end if;
     end if;
   end process;
   
-  process (y_r, a_r, b_r, output_reg_load) begin
-    if(output_reg_load = '1') then
-      y_nxt <= std_logic_vector(unsigned(a_r) + unsigned(b_r));
-    else
-      y_nxt <= x"00000000" & y_r(127 downto 32);
+  en <= output_reg_load or cnt(0) or cnt(1); 
+  
+  process (clk, reset_n) begin
+    if(reset_n = '0') then
+      y_r <= (others => '0');  
+      cnt <= (others => '0');   
+    elsif(clk'event and clk='1') then
+      if(en = '1') then
+        cnt <= std_logic_vector(unsigned(cnt) + 1);
+        if (cnt = "00") then
+            y_r <= std_logic_vector(unsigned('0' & a_rb(31 downto 0)) + unsigned('0'& b_rb(31 downto 0)) + unsigned(x"00000000"&y_r(32 downto 32)));
+        elsif(cnt = "01") then
+            y_r <= std_logic_vector(unsigned('0' & a_rb(63 downto 32)) + unsigned('0' & b_rb(63 downto 32)) + unsigned(x"00000000"&tmp(32 downto 32)));
+        elsif(cnt = "10") then
+            y_r <= std_logic_vector(unsigned('0' & a_rb(95 downto 64)) + unsigned('0' & b_rb(95 downto 64)) + unsigned(x"00000000"&y_r(32 downto 32))); 
+        elsif(cnt = "11") then
+            y_r <= std_logic_vector(unsigned('0' & a_rb(127 downto 96)) + unsigned('0' & b_rb(127 downto 96)) + unsigned(x"00000000"&y_r(32 downto 32)));  
+        end if;
+      else
+        y_r <= (others => '0');
+      end if;
     end if;
   end process;
   
-  data_out <= y_r(31 downto 0);
+  data_out <= tmp(31 downto 0) when cnt = "01" else
+                y_r(31 downto 0);
 
 end rtl;
